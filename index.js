ddfaa4e17b03a2afe2d7cc77afbbd8e7740dcb8f
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-02 22:43:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 import getArgs from "./helpers/args.js";
 import { printErr, printHelp, printSuccess } from "./services/log.service.js";
 import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async token => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token saved");
+  } catch (err) {
+    printErr(err.message);
+  }
+};
+
+const startCli = () => {
+  const args = getArgs(process.argv);
+
+  if (args.h) printHelp();
+
+  if (args.s) {
+    // save city
+  }
+
+  if (args.t) return saveToken(args.t);
+};
+
+startCli();
