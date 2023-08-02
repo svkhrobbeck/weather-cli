aa3ec967c05feca0c52666bf2723481f6b756c2b
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-02 13:27:05 +0500

    Create args helper

---
+++ helpers/args.js
@@ -0,0 +1,20 @@
+const getArgs = (args = []) => {
+  const res = {};
+  const rest = args.slice(2);
+
+  rest.forEach((val, idx, arr) => {
+    if (val.charAt() === "-") {
+      if (idx === arr.length - 1) {
+        res[val.substring(1)] = true;
+      } else if (arr[idx + 1].charAt(0) !== "-") {
+        res[val.substring(1)] = arr[idx + 1];
+      } else {
+        res[val.substring(1)] = true;
+      }
+    }
+  });
+
+  return res;
+};
+
+module.exports = getArgs;
